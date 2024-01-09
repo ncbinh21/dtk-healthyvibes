46a812849743f5f7f2baac8794fd01df93a261ac
Drake Nguyen <ncbinh21@gmail.com>
2024-01-09 21:36:07 +0700

    add option sent to GHN

--- app/code/Healthyvibes/GiaoHangNhanh/Observer/SaveShipmentAfterObserver.php
+++ app/code/Healthyvibes/GiaoHangNhanh/Observer/SaveShipmentAfterObserver.php
@@ -11,6 +11,7 @@ use Magento\Sales\Model\Order\Shipment\TrackFactory;
 use Healthyvibes\GiaoHangNhanh\Model\Carrier\GHN\Express;
 use Magento\Sales\Api\Data\ShipmentTrackInterface;
 use Psr\Log\LoggerInterface;
+use Magento\Framework\App\RequestInterface;
 
 /**
  * Class SaveShipmentAfterObserver
@@ -34,19 +35,27 @@ class SaveShipmentAfterObserver implements ObserverInterface
     private $logger;
 
     /**
+     * @var RequestInterface
+     */
+    private $request;
+
+    /**
      * SaveShipmentAfterObserver constructor.
      * @param CommandPoolInterface $commandPool
      * @param TrackFactory $trackFactory
      * @param LoggerInterface $logger
+     * @param RequestInterface $request
      */
     public function __construct(
         CommandPoolInterface $commandPool,
         TrackFactory $trackFactory,
-        LoggerInterface $logger
+        LoggerInterface $logger,
+        RequestInterface $request
     ) {
         $this->commandPool = $commandPool;
         $this->trackFactory = $trackFactory;
         $this->logger = $logger;
+        $this->request = $request;
     }
 
     /**
@@ -58,27 +67,28 @@ class SaveShipmentAfterObserver implements ObserverInterface
         $shipment = $observer->getEvent()->getShipment();
         /** @var \Magento\Sales\Model\Order $order */
         $order = $shipment->getOrder();
-
-        if (str_contains($order->getShippingMethod(), Config::GHN_CODE) || $order->getShippingMethod() == 'freeshipping_freeshipping') {
-            try {
-                $result = $this->commandPool->get('synchronize_order')->execute([
-                    'order' => $order,
-                    'service_id' => $order->getShippingAddress()->getShippingServiceId()
-                ]);
-                if ($result && $data = $result->get()) {
-                    if (isset($data['create_order'][AbstractDataBuilder::ORDER_CODE])) {
-                        $data = [
-                            ShipmentTrackInterface::CARRIER_CODE => Express::SERVICE_CODE,
-                            ShipmentTrackInterface::TITLE => Express::SERVICE_NAME,
-                            ShipmentTrackInterface::TRACK_NUMBER => $data['create_order'][AbstractDataBuilder::ORDER_CODE],
-                        ];
-                        $track = $this->trackFactory->create()->addData($data);
-                        $shipment->addTrack($track)->save();
+        if ($this->request->getParam('send_to_ghn')) {
+            if (str_contains($order->getShippingMethod(), Config::GHN_CODE) || $order->getShippingMethod() == 'freeshipping_freeshipping') {
+                try {
+                    $result = $this->commandPool->get('synchronize_order')->execute([
+                        'order' => $order,
+                        'service_id' => $order->getShippingAddress()->getShippingServiceId()
+                    ]);
+                    if ($result && $data = $result->get()) {
+                        if (isset($data['create_order'][AbstractDataBuilder::ORDER_CODE])) {
+                            $data = [
+                                ShipmentTrackInterface::CARRIER_CODE => Express::SERVICE_CODE,
+                                ShipmentTrackInterface::TITLE => Express::SERVICE_NAME,
+                                ShipmentTrackInterface::TRACK_NUMBER => $data['create_order'][AbstractDataBuilder::ORDER_CODE],
+                            ];
+                            $track = $this->trackFactory->create()->addData($data);
+                            $shipment->addTrack($track)->save();
+                        }
                     }
+                } catch (Exception $e) {
+                    $this->logger->error($e->getMessage());
+                    throw new Exception(__('We can\'t create shipment on GHN system. Please contact with admin.'));
                 }
-            } catch (Exception $e) {
-                $this->logger->error($e->getMessage());
-                throw new Exception(__('We can\'t create shipment on GHN system. Please contact with admin.'));
             }
         }
     }
